d013cb6e661cca6aa84809573042090344a08100
bishnu-prasad <bishnuprasadtripathy333@gmail.com>
2026-04-02 02:44:55 +0530

    fix bcrypt + password issue

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,7 +7,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Hash password
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 # Verify password (for login later)
 def verify_password(plain_password, hashed_password):
